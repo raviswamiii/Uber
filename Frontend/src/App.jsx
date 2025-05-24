2691dee9e8ddad1b217ecf72ac8587b6fd9c846c
raviswamiii <raviswamisamsung@gmail.com>
2025-05-24 19:25:50 +0530

    fixingBug

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Home } from './pages/Home'
 import { Start } from './pages/Start'
 import { UserLogin } from './pages/userLogin'
 import { UserRegister } from './pages/userRegister'
-import { CaptainLogin, captainLogin } from './pages/captainLogin'
+import { CaptainLogin } from './pages/captainLogin'
 import { CaptainRegister } from './pages/captainRegister'
 import { Route, Routes } from 'react-router-dom'
 
@@ -11,8 +11,8 @@ export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path={"/"} element={<Home/>} />
-        <Route path={"/start"} element={<Start/>} />
+        <Route path={"/"} element={<Start/>} />
+        <Route path={"/home"} element={<Home/>} />
         <Route path={"/userLogin"} element={<UserLogin/>} />
         <Route path={"/userRegister"} element={<UserRegister/>} />
         <Route path={"/captainLogin"} element={<CaptainLogin/>} />
