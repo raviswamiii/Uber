b5d85b4450c915ddeca909ac0f37ca2863618551
raviswamiii <raviswamisamsung@gmail.com>
2025-05-26 20:27:29 +0530

    captainLogin

--- Frontend/src/pages/CaptainLogin.jsx
+++ Frontend/src/pages/CaptainLogin.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import uberLogo from  "../assets/uber-logo.png"
+import uberLogo from "../assets/uber-logo.png";
 
 export const CaptainLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [captainData, setCaptainData] = useState({});
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const captain = {
+      email: email,
+      password: password,
+    };
+    setCaptainData(captain);
+
+    setEmail("");
+    setPassword("");
+  };
+
+  useEffect(() => {
+    console.log(captainData);
+  }, [captainData]);
   return (
     <div className="relative flex flex-col h-screen justify-between p-7">
-        <img className="absolute top-5 left-7 z-10 h-6" src={uberLogo} alt="" />
-      <form className="flex flex-col gap-8 mt-10" action="">
+      <img className="absolute top-5 left-7 z-10 h-6" src={uberLogo} alt="" />
+      <form onSubmit={(e)=>onSubmitHandler(e)} className="flex flex-col gap-8 mt-10" action="">
         <div className="flex flex-col gap-4">
           <div>
             <p className="text-lg font-semibold mb-1">Captain's email</p>
             <input
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
               className="w-full py-2 px-3 rounded outline-none bg-gray-200"
               type="text"
               placeholder="email@gmail.com"
@@ -19,6 +42,9 @@ export const CaptainLogin = () => {
           <div>
             <p className="text-lg font-semibold mb-1">Captain's password</p>
             <input
+              required
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
               className="w-full py-2 px-3 rounded outline-none bg-gray-200"
               type="password"
               placeholder="password"
@@ -26,10 +52,16 @@ export const CaptainLogin = () => {
           </div>
         </div>
         <div>
-            <button className="bg-black text-white w-full text-center py-2 rounded">
-          Login
-        </button>
-        <p className="text-center mt-1">New here? <Link to="/captainRegister" className="text-blue-600"> Register as a Captain</Link></p>
+          <button className="bg-black text-white w-full text-center py-2 rounded">
+            Login
+          </button>
+          <p className="text-center mt-1">
+            New here?{" "}
+            <Link to="/captainRegister" className="text-blue-600">
+              {" "}
+              Register as a Captain
+            </Link>
+          </p>
         </div>
       </form>
 
