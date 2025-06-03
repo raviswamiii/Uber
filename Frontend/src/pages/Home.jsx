7c6a1a16e154d51aedb65b1f27dbfa0ec19faab9
raviswamiii <raviswamisamsung@gmail.com>
2025-06-03 19:20:56 +0530

    Home

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,60 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import uberLogo from "../assets/uber-logo.png";
-import { LuSearch } from "react-icons/lu";
-import { HiMiniClock } from "react-icons/hi2";
+import { RiArrowDownWideFill } from "react-icons/ri";
 
 export const Home = () => {
+  const [openPanel, setOpenPanel] = useState(false);
   return (
-    <div className="p-4">
-      <img className="h-8 mb-7" src={uberLogo} alt="" />
+    <div className="relative h-screen w-full">
+      <img className="h-8 absolute left-4 top-3" src={uberLogo} alt="" />
 
-      <div className="flex flex-col gap-3">
-        <div className="flex bg-gray-200 items-center py-3 px-4 rounded-full gap-3">
-          <LuSearch className="text-2xl " />
-          <input
-            className="bg-transparent text-lg font-semibold placeholder-gray-600 outline-none"
-            type="text"
-            placeholder="Where to?"
-          />
-        </div>
-
-        <div className="flex flex-col gap-3">
-          <div className="flex items-center gap-3 px-3 py-4 border-2 rounded-lg">
-            <div className="bg-gray-200 p-2 rounded-lg">
-              <HiMiniClock className="text-lg" />
+      <div className="h-screen w-full">
+        <img
+          className="h-full w-full object-cover"
+          src="https://miro.medium.com/max/1280/0*gwMx05pqII5hbfmX.gif"
+          alt=""
+        />
+        <div className="absolute bottom-0 bg-white w-full ">
+          <form className="flex flex-col p-3 gap-3 h-[25vh] relative">
+            <div className="flex items-center mb-1">
+              <h1 className="text-xl font-semibold">Find a trip</h1>
+              <RiArrowDownWideFill
+                onClick={() => setOpenPanel(false)}
+                className={`${
+                  openPanel === false
+                    ? "absolute right-4 text-2xl text-gray-500 opacity-0"
+                    : "absolute right-4 text-2xl text-gray-500"
+                }`}
+              />
             </div>
-            <div className="leading-4 ">
-              <p className="">Chomu Pulia Circle</p>
-              <p className="text-sm text-gray-500 truncate w-[70vw]">
-                Chomu Pulia Cir, Ambabari, Vidyadhar Nagar, Jaipur
-              </p>
-            </div>
-          </div>
-
-          <div className="flex items-center gap-3 px-3 py-4 border-2 rounded-lg">
-            <div className="bg-gray-200 p-2 rounded-lg">
-              <HiMiniClock className="text-lg" />
-            </div>
-            <div className="leading-4 ">
-              <p className="">Shri Hari Ram Saboo Public School</p>
-              <p className="text-sm text-gray-500 truncate w-[70vw]">
-                WAQW+9Q7, Subash Nagar, Shastri Nagar, Jaipur
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div>
-        <div>
-          <h3>Suggestion</h3>
-          <p>See all</p>
-        </div>
-
-        <div>
-          <div>
-            <p>Courier</p>
-          </div>
+            <input
+              onClick={() => setOpenPanel(true)}
+              className="bg-[#eee] py-2 px-8 rounded-lg placeholder-gray-500 outline-yellow-500"
+              type="text"
+              placeholder="Add a pick-up location"
+            />
+            <input
+              onClick={() => setOpenPanel(true)}
+              className="bg-[#eee] py-2 px-8 rounded-lg placeholder-gray-500 outline-yellow-500 mb-5"
+              type="text"
+              placeholder="Enter your destination"
+            />
+            <div className="absolute h-14 w-[3px] bg-black rounded-full top-[42%] left-7"></div>
+          </form>
+          <div
+            className={`transition-all duration-500 ease-in-out  ${
+              openPanel ? "bg-white h-[75vh]" : "h-0"
+            }`}
+          ></div>
         </div>
       </div>
     </div>
